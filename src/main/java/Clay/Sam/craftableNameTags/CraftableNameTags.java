f09f11a3201616ac8090db9b6c61adbc90a62515
SaltyMini <samclaymichael@gmail.com>
2025-05-18 05:52:44 +0100

    code improvements

--- src/main/java/Clay/Sam/craftableNameTags/CraftableNameTags.java
+++ src/main/java/Clay/Sam/craftableNameTags/CraftableNameTags.java
@@ -7,12 +7,15 @@ import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class CraftableNameTags extends JavaPlugin {
 
     @Override
     public void onEnable() {
 
         Plugin plugin = Bukkit.getPluginManager().getPlugin("CraftableNameTags");
+        assert plugin != null;
         plugin.saveDefaultConfig();
 
         try {
@@ -21,22 +24,35 @@ public final class CraftableNameTags extends JavaPlugin {
             String line2 = plugin.getConfig().getString("recipe-line-2");
             String line3 = plugin.getConfig().getString("recipe-line-3");
 
-            ItemStack item1 = new ItemStack(Material.getMaterial(plugin.getConfig().getString("item-1")));
-            ItemStack item2 = new ItemStack(Material.getMaterial(plugin.getConfig().getString("item-2")));
-            ItemStack item3 = new ItemStack(Material.getMaterial(plugin.getConfig().getString("item-3")));
-            ItemStack item4 = new ItemStack(Material.getMaterial(plugin.getConfig().getString("item-4")));
-            ItemStack item5 = new ItemStack(Material.getMaterial(plugin.getConfig().getString("item-5")));
+            ItemStack item1 = new ItemStack(Objects.requireNonNull(Material.getMaterial(plugin.getConfig().getString("item-1"))));
+            ItemStack item2 = new ItemStack(Objects.requireNonNull(Material.getMaterial(plugin.getConfig().getString("item-2"))));
+            ItemStack item3 = new ItemStack(Objects.requireNonNull(Material.getMaterial(plugin.getConfig().getString("item-3"))));
+            ItemStack item4 = new ItemStack(Objects.requireNonNull(Material.getMaterial(plugin.getConfig().getString("item-4"))));
+            ItemStack item5 = new ItemStack(Objects.requireNonNull(Material.getMaterial(plugin.getConfig().getString("item-5"))));
+            ItemStack item6 = new ItemStack(Material.getMaterial(plugin.getConfig().getString("item-6")));
+            ItemStack item7 = new ItemStack(Material.getMaterial(plugin.getConfig().getString("item-7")));
+            ItemStack item8 = new ItemStack(Material.getMaterial(plugin.getConfig().getString("item-8")));
+            ItemStack item9 = new ItemStack(Material.getMaterial(plugin.getConfig().getString("item-9")));
+
+            if(line1 == null || line2 == null || line3 == null) {
+                throw new NullPointerException();
+            }
 
             ItemStack nameTag = new ItemStack(Material.NAME_TAG);
 
             try {
                 ShapedRecipe recipe = new ShapedRecipe(nameTag);
                 recipe.shape(line1, line2, line3);
-                recipe.setIngredient('1', item1.getType() != null ? item1.getType() : Material.BEDROCK);
-                recipe.setIngredient('2', item2.getType() != null ? item2.getType() : Material.BEDROCK);
-                recipe.setIngredient('3', item3.getType() != null ? item3.getType() : Material.BEDROCK);
-                recipe.setIngredient('4', item4.getType() != null ? item4.getType() : Material.BEDROCK);
-                recipe.setIngredient('5', item5.getType() != null ? item5.getType() : Material.BEDROCK);
+                item1.getType();
+                recipe.setIngredient('1', item1.getType());
+                recipe.setIngredient('2', item2.getType());
+                recipe.setIngredient('3', item3.getType());
+                recipe.setIngredient('4', item4.getType());
+                recipe.setIngredient('5', item5.getType());
+                recipe.setIngredient('6', item6.getType());
+                recipe.setIngredient('7', item7.getType());
+                recipe.setIngredient('8', item8.getType());
+                recipe.setIngredient('9', item9.getType());
                 getServer().addRecipe(recipe);
 
             } catch (NullPointerException e) {
@@ -49,8 +65,12 @@ public final class CraftableNameTags extends JavaPlugin {
             return;
 
             // Plugin startup logic
-
-
+        } catch (NullPointerException e) {
+            getLogger().warning("One or more of the recipe lines in the config.yml is invalid. Please check your config.yml file.");
+            return;
+        } catch (Exception e) {
+            getLogger().warning("An unknown error occurred. Please check your config.yml file.");
+            return;
         }
     }
 
